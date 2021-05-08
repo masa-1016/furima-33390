6a5f75a0e04e3760c44353503f93c0710c90dcb4
masa-1016 <77272798+masa-1016@users.noreply.github.com>
2021-05-08 17:56:07 +0900

    カテゴリー選択

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :postage
+  belongs_to_active_hash :area
+  belongs_to_active_hash :time
+
+  validates :nickname, :explanation, :price, presence: true
+  validates :category_id, :condition_id, :postage_id, :area_id, :time_id, numericality: { other_than: 1 }
 end
