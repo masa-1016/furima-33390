1f607837ee2835b84943ff552890e37238ce8b20
masa-1016 <77272798+masa-1016@users.noreply.github.com>
2021-05-22 16:59:58 +0900

    編集ページの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,21 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
    end
 
+   def edit
+    @item = Item.find(params[:id])
+   end
+
+   def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      @item = Item.find(params[:id])
+      render :edit
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :postage_id, :area_id, :delivery_id, :price, :image).merge(user_id: current_user.id)
