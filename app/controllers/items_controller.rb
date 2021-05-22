629a00fcd5b079350f40e3d17c472de410c801a7
masa-1016 <77272798+masa-1016@users.noreply.github.com>
2021-05-22 19:48:58 +0900

    非ログインユーザーのeditへ遷移不可

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-  before_action :move_to_index, only: [:edit]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.all.order(id: "DESC")
