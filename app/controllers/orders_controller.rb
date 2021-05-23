1673bbf1273e1e8e017ded05d49bbdfb96636bc6
masa-1016 <77272798+masa-1016@users.noreply.github.com>
2021-05-23 19:18:03 +0900

    購入画面のビューファイル作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,22 +2,22 @@ class OrdersController < ApplicationController
 
   def index
     @order = Order.new(order_params)
+    @item = Item.find(params[:item_id])
   end
 
   def create
     @order = Order.new(order_params)
-    if @order.valid?
-      @order.save
-      return redirect_to root_path
+    if @order.save
+      redirect_to root_path
     else
-      render 'index'
+      render :index
     end
   end
 
   private
 
   def order_params
-    params.permit(:postal, :area_id, :city, :address, :building, :telephone)
+    params.permit(:postal, :area_id, :city, :address, :building, :telephone, item_id: params[:item_id])
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,6 @@
 class Order < ApplicationRecord
+  #include ActiveModel::Model
+  #attr_accessor 
+
+  #validates :area_id { other_than: 1 }
 end
